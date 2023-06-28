276ed4a6dc18773b8f0566e4378fba5f0386b1e0
Tumelo Mokhwathi <51445929+Tumelo-Mokhwathi@users.noreply.github.com>
2023-06-28 12:02:15 +0200

    Update Client.cs

--- DataAccess/Models/Client.cs
+++ DataAccess/Models/Client.cs
@@ -3,9 +3,9 @@
     public class Client
     {
         public Guid ID { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public DateTime DateRegistered { get; set; }
-        public string Location { get; set; }
+        public string Location { get; set; } = string.Empty;
         public int NumberOfUsers { get; set; }
     }
 }
